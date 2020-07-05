e14b4e7ed2378434c191d5aa920af985e02f8492
Deepak Keshri <deepakkumarkeshri0613@gmail.com>
2020-07-05 09:59:47 +0530

    Updates

---
+++ static/js/20.fdaa9afe.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpvuexy-react-admin-dashboard"]=this["webpackJsonpvuexy-react-admin-dashboard"]||[]).push([[20],{1375:function(a,s,d){}}]);
+//# sourceMappingURL=20.fdaa9afe.chunk.js.map
