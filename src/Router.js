c9d49c2cd332abe7b5062578c0830bc549914996
Deepak Keshri <deepakkumarkeshri0613@gmail.com>
2020-07-05 10:58:58 +0530

    base url

--- src/Router.js
+++ src/Router.js
@@ -216,7 +216,7 @@ class AppRouter extends React.Component {
   render() {
     return (
       // Set the directory path if you are deploying in sub-folder
-      <Router history={history}>
+      <Router basename='/Demo/' history={history}>
         <Switch>
           <AppRoute exact path="/" component={analyticsDashboard} />
           <AppRoute
